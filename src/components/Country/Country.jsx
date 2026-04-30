00a09bbbb4339112b5ba9e1cf9879f8e8ab59ef8
Hello First Love <alveeafsi1234@gmail.com>
2026-05-01 03:13:41 +0600

    Add Visited Function

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import React from 'react'
 import './Country.css'
 
 function Country({country}) {
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(true);
+    }
+
     return (
         <div className='country'>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name : {country.name.common}</h3>
             <h5>Population : {country.population.population}</h5>
+            <p>Area : {country.area.area} { country.area.area > 30000 ? 'Big' : 'Small' }</p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Not Visited'}</button>
         </div>
     )
 }
